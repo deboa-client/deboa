5eeb0345b272122e826623788dc2f059b3f4c5dc
Rogério Araújo <rogerio.araujo@gmail.com>
2026-09-07 16:06:24 -0300

    chore: rename file to best match its purpose

---
+++ deboa-macros/tests/compile.rs
@@ -0,0 +1,9 @@
+#[test]
+fn ui() {
+    let t = trybuild::TestCases::new();
+    t.pass("tests/ui/resource-pass-1.rs");
+    t.compile_fail("tests/ui/resource-compile-fail-1.rs");
+    t.compile_fail("tests/ui/resource-compile-fail-2.rs");
+    t.compile_fail("tests/ui/resource-compile-fail-3.rs");
+    t.compile_fail("tests/ui/resource-compile-fail-4.rs");
+}
